295f67be77017dc5ba840deb5965f92ae2f7198c
asdhaliwal4 <asdhaliwal4@hotmail.com>
2026-03-20 16:24:23 +0000

    Update SignIn.jsx

--- react_frontend/src/components/SignIn.jsx
+++ react_frontend/src/components/SignIn.jsx
@@ -21,7 +21,7 @@ function SignIn({ handleLogin }) {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://final-year-project-iaod.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
